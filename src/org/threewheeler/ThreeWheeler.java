1c9ae9392f00d43f2ee43e412360c0443e8915ba
brindhavanan <brindhavanan@gmail.com>
2023-09-07 13:35:27 +0530

    updated ThreeWheeler.java file

--- src/org/threewheeler/ThreeWheeler.java
+++ src/org/threewheeler/ThreeWheeler.java
@@ -4,5 +4,7 @@ public class ThreeWheeler {
 	public void auto() {
 	System.out.println("Auto is used for travel 3 members");
 	}		
-	
+	public void Rikshaw(){
+	Sysytem.out.println("Rikshaw is used for travel 2 members");
+	}
 }
